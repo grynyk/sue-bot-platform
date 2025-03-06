485f1d08173ef06b735376fba07f67821cf921d3
grynyk <danielgrynyk@gmail.com>
2025-03-06 16:38:27 +0100

    fix port issue

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { Logger } from 'nestjs-pino';
 import { INestApplicationContext } from '@nestjs/common';
 
 async function bootstrap(): Promise<INestApplicationContext> {
-  const app: INestApplicationContext = await NestFactory.createApplicationContext(BotModule, {
+  const app = await NestFactory.create(BotModule, {
     bufferLogs: true,
     autoFlushLogs: false,
   });
   app.useLogger(app.get(Logger));
+  await app.listen(process.env.PORT || 3000);
   return app;
 }
 
