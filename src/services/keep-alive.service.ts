ecc661b5dec129ad2a3fbfef4eb9a374dd4ac045
grynyk <danielgrynyk@gmail.com>
2025-03-06 19:04:52 +0100

    keep-alive service implementation

---
+++ src/services/keep-alive.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import axios, { AxiosResponse } from 'axios';
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
+
+@Injectable()
+export class KeepAliveService {
+  private readonly url = process.env.HEROKU_APP_URL;
+
+  constructor(@InjectPinoLogger() protected readonly logger: PinoLogger) {}
+
+  @Cron(CronExpression.EVERY_30_MINUTES)
+  handleCron(): void {
+    this.logger.info(`Pinging ${this.url} to keep the dyno awake.`);
+    axios
+      .get(this.url)
+      .then((response: AxiosResponse): void => {
+        this.logger.info(`Ping successful. Status: ${response.status}`);
+      })
+      .catch((error): void => {
+        this.logger.error(`Ping failed: ${error.message}`);
+      });
+  }
+}
