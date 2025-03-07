2745aa57de91022b7b56290cbae3f4328e99ce83
grynyk <danielgrynyk@gmail.com>
2025-03-07 02:51:57 +0100

    store skintest result in db

--- src/modules/bot-user-data/bot-user-data.service.ts
+++ src/modules/bot-user-data/bot-user-data.service.ts
@@ -1,13 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { BotUser } from './entities/bot-user.entity';
 import { CreateBotUserDto } from './dto/create-bot-user.dto';
 import { UpdateBotUserDto } from './dto/update-bot-user.dto';
-
+import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 @Injectable()
 export class BotUserDataService {
-  constructor(@InjectRepository(BotUser) private readonly userRepository: Repository<BotUser>) {}
+  constructor(@InjectRepository(BotUser) private readonly userRepository: Repository<BotUser>,  @InjectPinoLogger() protected readonly logger: PinoLogger) {}
 
   create(createUserDto: Partial<CreateBotUserDto>): Promise<BotUser> {
     const user: BotUser = new BotUser();
@@ -33,20 +33,17 @@ export class BotUserDataService {
     return this.userRepository.findOneBy({ id });
   }
 
-  update(id: number, updateUserDto: UpdateBotUserDto): Promise<BotUser> {
-    const user: BotUser = new BotUser();
-    user.first_name = updateUserDto.first_name;
-    user.last_name = updateUserDto.last_name;
-    user.username = updateUserDto.username;
-    user.skin_type = updateUserDto.skin_type;
-    user.wake_up_time = updateUserDto.wake_up_time;
-    user.bed_time = updateUserDto.bed_time;
-    user.done_tasks_counter = updateUserDto.done_tasks_counter;
-    user.notifications_enabled = updateUserDto.notifications_enabled;
-    user.was_active_today = updateUserDto.was_active_today;
-    user.timestamp = updateUserDto.timestamp;
-    user.id = id;
-    return this.userRepository.save(user);
+  async update(id: number, updateUserDto: Partial<UpdateBotUserDto>): Promise<BotUser> {
+    try {
+      const user = await this.userRepository.findOneBy({ id });
+      if (!user) {
+        throw new NotFoundException(`User with id ${id} not found`);
+      }
+      Object.assign(user, updateUserDto);
+      return this.userRepository.save(user);
+    } catch (error) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
   }
 
   remove(id: number): Promise<{ affected?: number }> {
