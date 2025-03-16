08d05533275cdf4d63a053ec28ced8877201f659
grynyk <danielgrynyk@gmail.com>
2025-03-16 02:15:08 +0100

    import typeorm scheduler to worker module

--- src/workers/notifications/notifications-worker.module.ts
+++ src/workers/notifications/notifications-worker.module.ts
@@ -1,19 +1,41 @@
 import { Module } from '@nestjs/common';
-import { BotUserDataModule } from '@modules/bot-user-data';
+import { BotUser, BotUserDataModule } from '@modules/bot-user-data';
 import { NotificationDataModule } from '@modules/notification-data';
-import { PinoLogger } from 'nestjs-pino';
+import { LoggerModule, PinoLogger } from 'nestjs-pino';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { NotificationWorkerService } from './services/notifications-worker.service';
+import { GLOBAL_VARIABLES } from '@models/global.model';
+import { BotNotification } from '@modules/notification-data/entities/bot-notification.entity';
+import { PendingUserNotification } from '@modules/notification-data/entities/pending-user-notification.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { isDev, isStaging } from '@utils/env.util';
+import { LoggerOptions } from 'src/config/logger.options';
 
 @Module({
   imports: [
     TelegrafModule.forRoot({ token: process.env.BOT_TOKEN }),
+    ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRoot(LoggerOptions),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get<string>(GLOBAL_VARIABLES.DATABASE_URL),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+        entities: [BotUser, BotNotification, PendingUserNotification],
+        synchronize: true,
+        logging: isDev() || isStaging(),
+      }),
+    }),
     BotUserDataModule,
-    NotificationDataModule
+    NotificationDataModule,
   ],
-  providers: [ 
-    NotificationWorkerService,
-    PinoLogger,
-  ],
+  providers: [NotificationWorkerService, PinoLogger],
 })
 export class NotificationsWorkerModule {}
