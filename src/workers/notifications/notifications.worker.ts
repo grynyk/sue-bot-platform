81875f34d06be86eccadf52f7cb68b49f6704538
Daniel Grynyk <danielgrynyk@MacBook-G4W6677Q05-JLABS.local>
2025-03-16 01:28:20 +0100

    add types to notifications worker

--- src/workers/notifications/notifications.worker.ts
+++ src/workers/notifications/notifications.worker.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
 import { NotificationsWorkerModule } from './notifications-worker.module';
 import { NotificationWorkerService } from './services/notifications-worker.service';
+import { INestApplicationContext } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(NotificationsWorkerModule);
-  const logger = app.get(Logger);
+  const app: INestApplicationContext = await NestFactory.createApplicationContext(NotificationsWorkerModule);
+  const logger: Logger = app.get(Logger);
   app.useLogger(logger);
-  const workerService = app.get(NotificationWorkerService);
+  const workerService: NotificationWorkerService = app.get(NotificationWorkerService);
   await workerService.processNotifications();
 }
 
