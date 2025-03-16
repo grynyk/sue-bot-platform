1f9836fae879f8f8e59b7358ff46ffbcc53e13fc
Daniel Grynyk <danielgrynyk@MacBook-G4W6677Q05-JLABS.local>
2025-03-16 01:31:50 +0100

    fix notifications-worker.module imports

--- src/workers/notifications/notifications-worker.module.ts
+++ src/workers/notifications/notifications-worker.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
-import { BotUserDataService } from '@modules/bot-user-data';
-import { BotNotificationService, PendingUserNotificationService } from '@modules/notification-data';
+import { BotUserDataModule } from '@modules/bot-user-data';
+import { NotificationDataModule } from '@modules/notification-data';
 import { PinoLogger } from 'nestjs-pino';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { NotificationWorkerService } from './services/notifications-worker.service';
 
 @Module({
-  imports: [TelegrafModule.forRoot({ token: process.env.BOT_TOKEN })],
-  providers: [
-    BotUserDataService,
-    BotNotificationService,
-    PendingUserNotificationService,
+  imports: [
+    TelegrafModule.forRoot({ token: process.env.BOT_TOKEN }),
+    BotUserDataModule,
+    NotificationDataModule
+  ],
+  providers: [ 
     NotificationWorkerService,
     PinoLogger,
   ],
