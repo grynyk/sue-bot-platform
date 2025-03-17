9455c0e1d5e03cd3a00c46b63ea3bb0af17d02e4
Daniel Grynyk <danielgrynyk@MacBook-G4W6677Q05-JLABS.local>
2025-03-17 12:16:52 +0100

    rename globalState to SceneState

--- src/shared/scene-navigation/scene-navigation.ts
+++ src/shared/scene-navigation/scene-navigation.ts
@@ -2,7 +2,7 @@ import { Telegraf } from 'telegraf';
 import { Action, Ctx, InjectBot, SceneEnter, SceneLeave } from 'nestjs-telegraf';
 import { InjectPinoLogger, PinoLogger } from 'nestjs-pino';
 import { SceneStateService } from './services';
-import { GlobalStateItem } from './models';
+import { SceneStateItem } from './models';
 import { NAVIGATION_CALLBACK } from '@models/navigation.model';
 import { SCENE_ID, SceneContext } from '@models/scenes.model';
 import { triggerCallback } from '@utils/callback.utils';
@@ -54,7 +54,7 @@ export abstract class SceneNavigation {
   @SceneLeave()
   async onSceneLeave(@Ctx() ctx: SceneContext): Promise<void> {
     try {
-      const sceneData: GlobalStateItem = this.stateService.getSceneData();
+      const sceneData: SceneStateItem = this.stateService.getSceneData();
       if (sceneData && ctx.scene.current?.id === this.sceneId) {
         const messageId: number = this.stateService.getMessageId();
         if (messageId) {
