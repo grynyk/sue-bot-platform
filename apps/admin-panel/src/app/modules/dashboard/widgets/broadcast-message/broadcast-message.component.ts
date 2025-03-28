95aad6c9af409d9da75922f3054f1eb4b1f47f8a
grynyk <danielgrynyk@gmail.com>
2025-03-28 14:04:48 +0100

    webpack update, add empty broadcast message widget

---
+++ apps/admin-panel/src/app/modules/dashboard/widgets/broadcast-message/broadcast-message.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { InlineLoadingSpinnerComponent } from '../../../../shared';
+
+@Component({
+  standalone: true,
+  selector: 'sue-broadcast-message-widget',
+  styleUrls: ['broadcast-message.component.scss'],
+  templateUrl: 'broadcast-message.component.html',
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    InlineLoadingSpinnerComponent,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BroadcastMessageWidgetComponent {}
--- apps/admin-panel/webpack.config.js
+++ apps/admin-panel/webpack.config.js
@@ -4,8 +4,8 @@ const path = require('path');
 module.exports = {
   plugins: [
     new Dotenv({
-      path: path.resolve(__dirname, '../../.env'), // Load from monorepo root
-      systemvars: true, // Allows using system environment variables
+      path: path.resolve(__dirname, '../../.env'),
+      systemvars: true,
     }),
   ],
 };
