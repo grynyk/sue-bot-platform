3f3cb80689170419396c9d2a3efc58a43b80ffff
Daniel Grynyk <danielgrynyk@MacBook-G4W6677Q05-JLABS.local>
2025-03-28 20:19:13 +0100

    adjusted sizes of widgets

--- apps/admin-panel/src/app/modules/dashboard/widgets/bot-metrics/bot-metrics.component.ts
+++ apps/admin-panel/src/app/modules/dashboard/widgets/bot-metrics/bot-metrics.component.ts
@@ -43,7 +43,7 @@ export class BotMetricsWidgetComponent implements OnInit {
       group: ScaleType.Ordinal,
       domain: ['#007bff', '#28a745', '#ffc107', '#dc3545'],
     };
-    this.view = [window.innerWidth * 0.36, 415];
+    this.view = [window.innerWidth * 0.38, 415];
     this.isLoaded = false;
   }
 
