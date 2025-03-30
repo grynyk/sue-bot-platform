936ea8a547a06356a65f78a0ec5f282dc6398b11
grynyk <danielgrynyk@gmail.com>
2025-03-30 04:47:22 +0200

    adjust height of widget [17]

--- apps/admin-panel/src/app/modules/dashboard/widgets/bot-users-metrics/bot-users-metrics.component.ts
+++ apps/admin-panel/src/app/modules/dashboard/widgets/bot-users-metrics/bot-users-metrics.component.ts
@@ -60,8 +60,8 @@ export class BotUsersMetricsWidgetComponent implements OnInit {
 
   private setChartConfig(): void {
     const isMobile: boolean = this.screenSizeService.isMobile();
-    const width: number = isMobile ? window.innerWidth * 0.95 : window.innerWidth * 0.25;
-    const height: number = isMobile ? 205 : 410;
+    const width: number = isMobile ? window.innerWidth * 0.92 : window.innerWidth * 0.25;
+    const height: number = isMobile ? 205 : 350;
     this.view = [width, height];
     this.colorScheme = {
       name: 'customScheme',
